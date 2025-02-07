0835e0c7419034d4a5f2515795c5fc3f5855a4e3
braispn <braispose15@gmail.com>
2025-02-07 12:56:56 +0100

    .

--- PROG_2024/Java/t04_01_Pose_Nieto/exer9/src/concesionario/Vehiculo.java
+++ PROG_2024/Java/t04_01_Pose_Nieto/exer9/src/concesionario/Vehiculo.java
@@ -2,21 +2,36 @@ package concesionario;
 
 import utils.TipoCombustible;
 
-/**
- * Clase que representa un vehículo.
- */
 public class Vehiculo {
     private static int numeroTotalVehiculos = 0;
-    private static int velocidadeMaxima = 120;
-
+    private static double velocidadeMaxima = 120;
     private String matricula;
     private String marca;
     private String modelo;
     private TipoCombustible tipoCombustible;
-    private int velocidade;
+    private double velocidade;
 
     public static boolean comprobarMatricula(String matricula) {
-        return matricula.matches("\\d{4}[BCDFGHJKLMNPRSTVWXYZ]{3}");
+        if (matricula == null || matricula.length() != 7) {
+            return false;
+        }
+
+        // Verificar os primeiros 4  díxitos
+        for (int i = 0; i < 4; i++) {
+            if (matricula.charAt(i) < '0' || matricula.charAt(i) > '9') {
+                return false;
+            }
+        }
+
+        // Verificar os últimos 3 caracteres
+        String letrasValidas = "BCDFGHJKLMNPRSTVWXYZ";
+        for (int i = 4; i < 7; i++) {
+            if (letrasValidas.indexOf(matricula.charAt(i)) == -1) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public Vehiculo(String matricula, String marca, String modelo) {
@@ -34,19 +49,30 @@ public class Vehiculo {
     }
 
     private void modificarVelocidade(int cambio) {
-        this.velocidade = Math.max(0, Math.min(velocidadeMaxima, this.velocidade + cambio));
+        this.velocidade = this.velocidade + cambio;
+    
+        if (this.velocidade < 0) {
+            this.velocidade = 0;
+        }
+    
+        if (this.velocidade > velocidadeMaxima) {
+            this.velocidade = velocidadeMaxima;
+        }
     }
+    
 
     public void acelerar() {
         modificarVelocidade(10);
     }
-
+    /**
+     * Método para acelerar.
+     */
     public void acelerar(int velocidade) {
         modificarVelocidade(velocidade);
     }
 
     /**
-     * Método para decelerar en 10 km/h.
+     * Método para decelerar 10 km/h.
      */
     public void decelerar() {
         modificarVelocidade(-10);
@@ -74,7 +100,7 @@ public class Vehiculo {
         return tipoCombustible;
     }
 
-    public int getVelocidade() {
+    public double getVelocidade() {
         return velocidade;
     }
 
@@ -82,11 +108,11 @@ public class Vehiculo {
         return numeroTotalVehiculos;
     }
 
-    public static int getVelocidadeMaxima() {
+    public static double getVelocidadeMaxima() {
         return velocidadeMaxima;
     }
 
-    public static void setVelocidadeMaxima(int novaVelocidadeMaxima) {
+    public static void setVelocidadeMaxima(double novaVelocidadeMaxima) {
         velocidadeMaxima = novaVelocidadeMaxima;
     }
 }
