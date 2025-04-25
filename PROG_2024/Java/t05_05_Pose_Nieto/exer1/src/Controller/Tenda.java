67a1d9286e213037ee2668ac2019ee3d2ce8e257
braispn <braispose15@gmail.com>
2025-04-25 12:59:20 +0200

    Musi.com menu y funciones tienda para facer

--- PROG_2024/Java/t05_05_Pose_Nieto/exer1/src/Controller/Tenda.java
+++ PROG_2024/Java/t05_05_Pose_Nieto/exer1/src/Controller/Tenda.java
@@ -1,14 +1,21 @@
-package Model;
+package Controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
+import Model.Administrador;
+import Model.Cliente;
+import Model.Usuario;
 import Utils.RolUsuario;
+import View.Vista;
 
 public class Tenda {
     private static Tenda tenda;
+    private Vista vista;
     private ArrayList<Usuario> usuarios;
     private ArrayList<Administrador> admins;
     private ArrayList<Cliente> clientes;
+    private Usuario usuarioActual = null;
 
     
 
@@ -16,6 +23,8 @@ public class Tenda {
         this.usuarios = new ArrayList<Usuario>();
         this.admins = new ArrayList<Administrador>();
         this.clientes = new ArrayList<Cliente>();
+
+        rexistrarUsuario("Pepe", "abc123.", RolUsuario.ADMINISTRADOR);
     }
 
     public static Tenda getInstancia() {
@@ -37,4 +46,28 @@ public class Tenda {
         }
 
     }
+
+    public boolean comprobarUsuario(HashMap<String,String> sesion){
+        for(Usuario u: usuarios){
+            if (sesion.containsKey(u.getNome()) && sesion.containsValue(u.getContrasinal())) {
+                this.usuarioActual = u;
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public void engadirProducto(){
+        
+    }
+
+    public void cerrarSesion(){
+        this.usuarioActual = null;
+    }
+
+    public Usuario getUsuarioActual() {
+        return this.usuarioActual;
+    }
+
+    
 }
--- PROG_2024/Java/t05_05_Pose_Nieto/exer1/src/View/Vista.java
+++ PROG_2024/Java/t05_05_Pose_Nieto/exer1/src/View/Vista.java
@@ -1,9 +1,11 @@
 package View;
 
+import java.util.Scanner;
+
 public class Vista {
 
     public void inicio() {
-        System.out.println("**************************");
+        System.out.println("\n**************************");
         System.out.println("**************************");
         System.out.println("*** BENVIDO A MUSI.COM ***");
         System.out.println("**************************");
@@ -11,21 +13,59 @@ public class Vista {
         System.out.println("\nDesea iniciar sesión (s/n)");
     }
 
-    public void inicioSesion() {
-        System.out.println("\nUsuario -> ");
-        System.out.println("Contrasinal ->");
-    }
+    // public HashMap<String, String> inicioSesion(Scanner s) {
+    //     System.out.print("\nUsuario -> ");
+    //     String Usuario = s.nextLine();
+    //     System.out.print("Contrasinal -> ");
+    //     String Contrasinal = s.nextLine();
+    //     HashMap<String, String> sesion = new HashMap<>();
+    //     sesion.put(Usuario, Contrasinal);
+    //     return sesion;
+    // }
 
     public void menuAdministrador() {
         System.out.println("1) Engadir produto");
         System.out.println("2) Ver produtos");
         System.out.println("3) Engadir stock");
         System.out.println("4) Eliminar stock");
+        System.out.println("5) Cerrar Sesión");
+        System.out.print("\nOpción -> ");
+
 
         // Engadir produto (submenú para escoller tipo)
         // Ver produtos (submenú):
         // Engadir stock (por identificador)
         // Eliminar stock (por identificador)
+    }
+    
+    public void menuEngadirProducto(Scanner s){
+        System.out.println("1) Instrumento");
+        System.out.println("2) Complemento");
+        String opcion = s.nextLine();
+        switch (opcion) {
+            case "1":
+                System.out.println("1) Saxofón");
+                System.out.println("2) Frauta");
+                System.out.println("3) Trombón");
+                opcion = s.nextLine();
+                switch (opcion) {
+                    case "1":
+                        
+                        break;
+                    case "2":
+
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case "2":
+                
+                break;
+            default:
+                System.out.println("Error: Escolla unha opción disponible");
+                break;
+        }
     }
 
     public void menuProductosAdmin() {
@@ -38,14 +78,21 @@ public class Vista {
         System.out.println("7) Tódolos libros");
         System.out.println("8) Tódolos estoxos");
         System.out.println("9) Información dun produto concreto (por identificador)");
+        System.out.println("10) Sair");
+        System.out.print("\nOpción -> ");
+
     }
 
     public void menuCliente() {
         System.out.println("1) Ver produtos");
         System.out.println("2) Comprar unha unidade");
+        System.out.println("3) Cerrar Sesión");
+        System.out.print("\nOpción -> ");
+
 
         // Ver produtos (submenú)
-        // Comprar unha unidade (por identificador, sen almacenar compras, só modificar stock)
+        // Comprar unha unidade (por identificador, sen almacenar compras, só modificar
+        // stock)
     }
 
     public void menuProductosCliente() {
@@ -57,5 +104,7 @@ public class Vista {
         System.out.println("6) Tódolos complementos");
         System.out.println("7) Tódolos libros");
         System.out.println("8) Tódolos estoxos");
+        System.out.println("9) Sair");
+        System.out.print("\nOpción -> ");
     }
 }
